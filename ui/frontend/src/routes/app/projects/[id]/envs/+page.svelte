d1a0b7d50655d05270c4e0b687e911a46cdf4632
Jan Lauber <jan.lauber@protonmail.ch>
2023-12-18 12:54:24 +0100

    feat: Add environment variables and accordion component
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- ui/frontend/src/routes/app/projects/[id]/envs/+page.svelte
+++ ui/frontend/src/routes/app/projects/[id]/envs/+page.svelte
@@ -1,5 +1,27 @@
-<script>
-  import { Heading, P } from "flowbite-svelte";
+<script lang="ts">
+  import { rollouts } from "$lib/stores/data";
+  import { Accordion, AccordionItem, Button, Heading, Input, Label, P } from "flowbite-svelte";
+  import { Lock, Variable } from "lucide-svelte";
+
+  interface Env {
+    id: string;
+    name: string;
+    value: string;
+  }
+
+  let envs: Env[] = [
+    {
+      id: "1",
+      name: "TEST",
+      value: "test"
+    },
+    {
+      id: "2",
+      name: "TEST2",
+      value: "test2"
+    }
+  ];
+  let secrets: Env[] = [];
 </script>
 
 <div class="flex items-start justify-between">
@@ -10,3 +32,54 @@
     </P>
   </div>
 </div>
+
+<Accordion class="gap-2 grid mt-10">
+  {#key $rollouts}
+    <AccordionItem class="rounded-lg">
+      <div slot="header" class="flex">
+        <div class="ring-1 p-2 rounded-lg ring-gray-500 mr-2 flex items-center justify-center">
+          <Variable class="w-4 h-4" />
+        </div>
+        <span class="pt-1">Environment Variables</span>
+      </div>
+      {#each envs as env, i (env.id)}
+        <div class="flex items-end space-x-4">
+          <div class="flex-grow grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <Label class="mt-4">Name</Label>
+              <Input
+                size="sm"
+                type="text"
+                placeholder="Name"
+                bind:value={env.name}
+                class="w-full"
+              />
+            </div>
+            <div>
+              <Label class="mt-4">Value</Label>
+              <Input
+                size="sm"
+                type="text"
+                placeholder="Value"
+                bind:value={env.value}
+                class="w-full"
+              />
+            </div>
+          </div>
+          <div>
+            <Button class="bg-red-500 hover:bg-red-600" size="sm">Delete</Button>
+          </div>
+        </div>
+      {/each}
+    </AccordionItem>
+    <AccordionItem class="rounded-lg">
+      <div slot="header" class="flex">
+        <div class="ring-1 p-2 rounded-lg ring-gray-500 mr-2 flex items-center justify-center">
+          <Lock class="w-4 h-4" />
+        </div>
+        <span class="pt-1">Secret Environment Variables</span>
+      </div>
+      <div class=""></div>
+    </AccordionItem>
+  {/key}
+</Accordion>
