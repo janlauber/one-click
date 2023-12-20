679957076a7cfc3bc7bce818c182206372a08ec3
Jan Lauber <jan.lauber@protonmail.ch>
2023-12-20 23:13:31 +0100

    feat: Refactor image controller and auto-update logic
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- ui/pocketbase/pkg/controller/image.go
+++ ui/pocketbase/pkg/controller/image.go
@@ -1,20 +1,90 @@
 package controller
 
 import (
+	"io"
 	"log"
+	"strings"
+
+	"encoding/json"
 
 	"github.com/labstack/echo/v5"
 	"github.com/natrontech/one-click/pkg/image"
 	"github.com/pocketbase/pocketbase"
 )
 
+type UpdateRequest struct {
+	Pattern    string `json:"pattern"`
+	Policy     string `json:"policy"`
+	Registry   string `json:"registry"`
+	Repository string `json:"repository"`
+}
+
 func HandleAutoUpdate(c echo.Context, app *pocketbase.PocketBase) error {
 	// call image.AutoUpdate
-	err := image.AutoUpdate(app)
+
+	// read the request body
+	body, err := io.ReadAll(c.Request().Body)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 
-	return c.JSON(200, "ok")
+	// parse the request body
+	var updateReq UpdateRequest
+	err = json.Unmarshal(body, &updateReq)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// get the filter tags (pattern, extract) from the request body
+	pattern := updateReq.Pattern // required
+	if pattern == "" {
+		pattern = ".*"
+	}
+
+	// check if the pattern contains spaces and remove them
+	pattern = strings.ReplaceAll(pattern, " ", "")
+
+	// get the policy (semver (range), alphabetical (asc, desc), latest) from the request body
+	policy := updateReq.Policy // required
+	if policy == "" {
+		policy = "semver"
+	}
+
+	// get the registry from the request body
+	registry := updateReq.Registry // required
+	if registry == "" {
+		registry = "https://registry.hub.docker.com/v2/repositories"
+	}
+
+	// get the repository from the request body
+	repository := updateReq.Repository // required
+	if repository == "" {
+		repository = "janlauber/test-image"
+	}
+
+	filterTags := image.FilterTags{
+		Pattern: pattern,
+		Policy:  policy,
+	}
+
+	tags, err := image.FilterAndSortTags(app, registry, repository, filterTags)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// if no tags are found, return an empty response
+	var latestTag string
+	if len(tags) > 0 {
+		latestTag = tags[0]
+	} else {
+		tags = []string{}
+	}
+
+	return c.JSON(200, map[string]interface{}{
+		"tags":      tags,
+		"latestTag": latestTag,
+	})
 }
