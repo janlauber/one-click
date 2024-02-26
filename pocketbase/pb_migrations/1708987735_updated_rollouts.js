e86a1d3f3ab6cfb7c671872c0620a697dbf10e7f
Jan Lauber <jan.lauber@protonmail.ch>
2024-02-27 00:29:22 +0100

    feat: add hidden and fix rollout behaviour
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

---
+++ pocketbase/pb_migrations/1708987735_updated_rollouts.js
@@ -0,0 +1,27 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("22k6ts6gvnp46mc")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "jv7hnbca",
+    "name": "hide",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("22k6ts6gvnp46mc")
+
+  // remove
+  collection.schema.removeField("jv7hnbca")
+
+  return dao.saveCollection(collection)
+})
