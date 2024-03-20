6005216255415c0976317f8a8d13afe707e6a0c1
Jan Lauber <jan.lauber@protonmail.ch>
2024-03-20 15:51:30 +0100

    feat: show ID on project card
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- frontend/src/lib/components/dashboard/ProjectCard.svelte
+++ frontend/src/lib/components/dashboard/ProjectCard.svelte
@@ -147,6 +147,13 @@
     </div>
   </div>
   <dl class="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+    <div class="flex justify-between gap-x-4 py-3">
+      <dt class="">ID</dt>
+      <dd class="flex items-start gap-x-2">
+        <span class="cursor-default">{project.id}</span>
+      </dd>
+    </div>
+
     {#if these_rollouts.length > 0}
       <div class="flex justify-between gap-x-4 py-3">
         <dt class="">Last rollout</dt>
