55c735bd2036d57d28a478066ee7099c60833ee3
Jan Lauber <jan.lauber@protonmail.ch>
2024-04-21 16:08:07 +0200

    feat: Add rollout functionality
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

---
+++ frontend/src/routes/app/projects/[id]/deployments/[id]/rollouts/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Heading, P } from "flowbite-svelte";
+  import RolloutsTable from "$lib/components/rollouts/RolloutsTable.svelte";
+</script>
+
+<div class="flex items-start justify-between">
+  <div class="flex flex-col">
+    <Heading tag="h2">Rollouts</Heading>
+    <P class="text-gray-500 dark:text-gray-400 text-sm">
+      Each time you change the configuration of a project, a new rollout is created.
+    </P>
+  </div>
+</div>
+
+<div>
+  <RolloutsTable />
+</div>
